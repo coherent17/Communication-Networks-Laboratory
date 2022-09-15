1471e353413f1fe69e8ed363274aee98c3538bdd
coherent <mnb51817@gmail.com>
2022-09-15 15:40:01 +0800

    optimize the function

--- Lab0/src/0811562_1.py
+++ Lab0/src/0811562_1.py
@@ -1,47 +1,34 @@
-def even_digit_X_2(card_numbers):
-    result = []
-    for number in card_numbers:
-        count = 0
-        for i in range(len(number) - 1, -1, -1):
-            count = count + 1
+def credit_check(full_number):
 
-            #get the even digit
-            if count % 2 == 0:
-                result.append(int(number[i]) * 2)
+    #using flag to check whether is even/odd digit
+    isEven = False
+    sum = 0
 
-    return result
+    for i in range(len(full_number) - 1, -1, -1):
+        digit = int(full_number[i])
+        
+        if isEven:
+            digit *= 2
+        
+        sum += digit // 10      #for odd digit, +0
+        sum += digit % 10
 
-def sum_digit_in_array(even_digit_sum_X_2):
-    result = 0
-    for x in even_digit_sum_X_2:
-        for digit in str(x):
-            result += int(digit)
-    return result
-
-def sum_odd_digit(card_numbers):
-    result = 0
-    for number in card_numbers:
-        count = 0
-        for i in range(len(number) - 1, -1, -1):
-            count = count + 1
-
-            #get the odd digit
-            if count % 2 == 1:
-                result += int(number[i])
-
-    return result
-
-# read how many data to check for validation
-n = int(input())
-
-for i in range(n):
-    card_numbers = list(map(str, input().split(" ")))
-    even_digit_sum_X_2 = even_digit_X_2(card_numbers)
-    digit_sum = sum_digit_in_array(even_digit_sum_X_2)
-    odd_digit_sum = sum_odd_digit(card_numbers)
-    total = digit_sum + odd_digit_sum
-
-    if(total % 10 == 0):
+        isEven = ~isEven
+    
+    if sum % 10 == 0:
         print("Valid")
     else:
         print("Invalid")
+
+        
+n = int(input())
+
+for i in range(n):
+    cards_number = list(map(str, input().split(" ")))
+
+    #concatenate the string without space
+    full_number = ""
+    for number in cards_number:
+        full_number = full_number + number
+    
+    credit_check(full_number)
