b92b6aeeabf9f37896c03dc28bea87113c4a4913
coherent <mnb51817@gmail.com>
2022-09-15 16:22:33 +0800

    finish dp

--- Lab0/src/0811562_3.py
+++ Lab0/src/0811562_3.py
@@ -1,12 +1,17 @@
-#dynamic programming
+#dynamic programming bottom up with tabulation
+
+# define dp[i] = the min # of the coins to sum up to i
+# base case: dp[0] = 0
+# transition function: dp[i] = min(dp[currAmount], 1 + dp[currAmount - coin])
 def coinChange(coins, amount):
-    dp = [float('inf')] * (amount + 1)
+    dp = [float('inf')] * (amount + 1) # 0 ~ amount
     dp[0] = 0
 
-    for coin in coins:
-        for currAmount in range(1, amount + 1):
-            if currAmount >= coin:
-                dp[currAmount] = min(dp[currAmount], 1 + dp[currAmount - coin])
+    #calculate every value in dp array (bottom up)
+    for currAmount in range(1, amount + 1):
+        for c in coins:
+            if currAmount - c >= 0:
+                dp[currAmount] = min(dp[currAmount], 1 + dp[currAmount - c])
 
     return dp[amount] if dp[amount] != float('inf') else -1
 
@@ -17,5 +22,6 @@ for i in range(n):
     coinsSize, amount = input().split(" ")
     amount = int(amount)
     coins = list(map(int, input().split(" ")))
+    coins.sort()    #just to make sure the coins be sorted
     result = coinChange(coins, amount)
     print(result)
