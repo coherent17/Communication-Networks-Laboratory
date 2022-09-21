c415eae78bd37d2c85c084470dd159d0a242eb1b
coherent17 <mnb51817@gmail.com>
2022-09-21 21:01:13 +0800

    setup sh

---
+++ IOT_Lab1/Q2/setup.sh
@@ -0,0 +1,5 @@
+$   sudo apt-get update
+$   sudo apt-get install git-core build-essential python-dev
+$   git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+$   cd Adafruit_Python_DHT
+$   sudo python setup.py install 
