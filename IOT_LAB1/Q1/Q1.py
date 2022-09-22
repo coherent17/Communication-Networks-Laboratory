f7e2bca6368ef0826773b5b625ca68a4a4d00a99
coherent17 <mnb51817@gmail.com>
2022-09-22 08:10:35 +0100

    update Q1

--- IOT_LAB1/Q1/Q1.py
+++ IOT_LAB1/Q1/Q1.py
@@ -11,15 +11,11 @@ unit = 0.3
 
 #short in Morse Code
 def dot():
-    GPIO.output(LED_PIN, GPIO.HIGH)
-    time.sleep(1 * unit)
-    GPIO.output(LED_PIN, GPIO.LOW)
+    GPIO.output(LED_PIN, GPIO.HIGH); time.sleep(1 * unit); GPIO.output(LED_PIN, GPIO.LOW)
 
 #long in Morse Code
 def dash():
-    GPIO.output(LED_PIN, GPIO.HIGH)
-    time.sleep(3 * unit)
-    GPIO.output(LED_PIN, GPIO.LOW)
+    GPIO.output(LED_PIN, GPIO.HIGH); time.sleep(3 * unit); GPIO.output(LED_PIN, GPIO.LOW)
 
 def space_between_same_letter():
     time.sleep(1 * unit)
@@ -29,50 +25,37 @@ def space_between_letter():
 
 def N():
     print('N')
-    dash()
-    space_between_same_letter()
+    dash(); space_between_same_letter()
     dot()
 
 def Y():
     print('Y')
-    dash()
-    space_between_same_letter()
-    dot()
-    space_between_same_letter()
-    dash()
-    space_between_same_letter()
+    dash(); space_between_same_letter()
+    dot();  space_between_same_letter()
+    dash(); space_between_same_letter()
     dash()
 
 def C():
     print('C')
-    dash()
-    space_between_same_letter()
-    dot()
-    space_between_same_letter()
-    dash()
-    space_between_same_letter()
+    dash(); space_between_same_letter()
+    dot();  space_between_same_letter()
+    dash(); space_between_same_letter()
     dot()
 
 def U():
     print('U')
-    dot()
-    space_between_same_letter()
-    dot()
-    space_between_same_letter()
+    dot(); space_between_same_letter()
+    dot(); space_between_same_letter()
     dash()
 
 
 #main driven code
 if __name__ == "__main__":
 
-    N()
-    space_between_letter()
-    Y()
-    space_between_letter()
-    C()
-    space_between_letter()
-    U()
-    space_between_letter()
+    N();    space_between_letter()
+    Y();    space_between_letter()
+    C();    space_between_letter()
+    U();    space_between_letter()
 
     #wait seven unit to stop the program
     time.sleep(7 * unit)
