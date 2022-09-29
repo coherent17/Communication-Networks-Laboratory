4ea294c28f4491d6db192037fb6069928f8a1a96
coherent17 <mnb51817@gmail.com>
2022-09-29 11:49:17 +0800

    Q3 src

--- IOT_LAB2/Q3/client.py
+++ IOT_LAB2/Q3/client.py
@@ -1,12 +1,12 @@
 import bluetooth
 
-bd_addr="DC:A6:32:22:2C:B3"
+bd_addr="DC:A6:32:11:A3:66"
 
 port = 2
 
 sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((bd_addr, port))
 
-sock.send("hello!!")
+sock.send("client_0811562 & server 109700035")
 
 sock.close()
--- IOT_LAB2/Q3/server.py
+++ IOT_LAB2/Q3/server.py
@@ -2,10 +2,10 @@ import bluetooth
 
 server_sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 port=2
-server_sock.blind(("DC:A6:32:22:2C:B3", port))
+server_sock.bind(("DC:A6:32:22:2C:B3", port))
 server_sock.listen(1)
 client_sock,address=server_sock.accept()
-print "Accepted connection form ", address
+print "Accepted connection from ", address
 
 data = client_sock.recv(1024)
 print "receiverd [%s]" % data
