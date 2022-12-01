d0556d2299d9d9e5b2578296b9f46f48c1444c80
coherent <mnb51817@gmail.com>
2022-12-01 11:20:45 +0800

    update Q3 comment

--- Android_Python_Programming_LAB1/APP_Q3/app/src/main/java/com/example/app_q3/MainActivity.java
+++ Android_Python_Programming_LAB1/APP_Q3/app/src/main/java/com/example/app_q3/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.app_q3;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -9,9 +7,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-
-
 public class MainActivity extends AppCompatActivity {
+
+    //variable declaration
     TextView TopText;
     Button setbutton;
     Button resetbutton;
@@ -21,11 +19,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //connect object by ID
         TopText = (TextView)findViewById(R.id.TopText);
         setbutton = (Button)findViewById(R.id.setbutton);
         resetbutton = (Button)findViewById(R.id.resetbutton);
         Name = (EditText)findViewById(R.id.Name);
 
+
+        //set button: show the "Welcome to Android" with the string on EditText
         setbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -33,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        //reset button: reset the string in TopText to "Hello World"
         resetbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
