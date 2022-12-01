d2daa0ffd928a3634c1f920dfcb4630945469e94
coherent <mnb51817@gmail.com>
2022-12-01 10:53:08 +0800

    add comment

--- Android_Python_Programming_LAB1/APP0811562/app/src/main/java/com/example/app0811562/MainActivity.java
+++ Android_Python_Programming_LAB1/APP0811562/app/src/main/java/com/example/app0811562/MainActivity.java
@@ -5,11 +5,12 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    //variable declaration
     TextView showtext;
     Button demobutton;
     Button backbutton;
@@ -18,9 +19,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //connect the object by ID
         showtext = (TextView)findViewById(R.id.showtext);
         demobutton = (Button)findViewById(R.id.demobutton);
         backbutton = (Button)findViewById(R.id.backbutton);
+
+
+        //demo button: increase text size and show "Pass"
         demobutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -32,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
+        //back button: decrease text size and show "Android Lab1 demo"
         backbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
