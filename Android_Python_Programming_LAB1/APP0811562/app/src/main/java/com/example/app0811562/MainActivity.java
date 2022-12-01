d0400a700c1c939325dd4fa6b7ed67101a015da2
coherent <mnb51817@gmail.com>
2022-12-01 15:28:48 +0800

    add src

--- Android_Python_Programming_LAB1/APP0811562/app/src/main/java/com/example/app0811562/MainActivity.java
+++ Android_Python_Programming_LAB1/APP0811562/app/src/main/java/com/example/app0811562/MainActivity.java
@@ -5,28 +5,25 @@ import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    //variable declaration
     TextView showtext;
     Button demobutton;
     Button backbutton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //connect the object by ID
         showtext = (TextView)findViewById(R.id.showtext);
         demobutton = (Button)findViewById(R.id.demobutton);
         backbutton = (Button)findViewById(R.id.backbutton);
 
 
-        //demo button: increase text size and show "Pass"
         demobutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -38,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        //back button: decrease text size and show "Android Lab1 demo"
+
         backbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
--- Android_Python_Programming_LAB1/APP_Q3/app/src/main/java/com/example/app_q3/MainActivity.java
+++ Android_Python_Programming_LAB1/APP_Q3/app/src/main/java/com/example/app_q3/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.app_q3;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.View;
@@ -7,9 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
 
-    //variable declaration
+
+public class MainActivity extends AppCompatActivity {
     TextView TopText;
     Button setbutton;
     Button resetbutton;
@@ -19,15 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        //connect object by ID
         TopText = (TextView)findViewById(R.id.TopText);
         setbutton = (Button)findViewById(R.id.setbutton);
         resetbutton = (Button)findViewById(R.id.resetbutton);
         Name = (EditText)findViewById(R.id.Name);
 
-
-        //set button: show the "Welcome to Android" with the string on EditText
         setbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
@@ -35,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //reset button: reset the string in TopText to "Hello World"
+
         resetbutton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
