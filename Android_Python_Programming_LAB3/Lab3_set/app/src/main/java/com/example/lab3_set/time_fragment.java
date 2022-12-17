3904fbbd8e26e0604284cadc64bcb9131719d47c
coherent <mnb51817@gmail.com>
2022-12-17 15:11:44 +0800

    rm space

--- Android_Python_Programming_LAB3/Lab3_set/app/src/main/java/com/example/lab3_set/time_fragment.java
+++ Android_Python_Programming_LAB3/Lab3_set/app/src/main/java/com/example/lab3_set/time_fragment.java
@@ -1,10 +1,8 @@
 package com.example.lab3_set;
-
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
